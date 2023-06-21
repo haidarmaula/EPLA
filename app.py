4a2b7410e929da8615e132b806113f40c85a253b
haidarmaula <haidarmaulareynaldy@gmail.com>
2023-06-21 22:05:53 +0700

    Konfigurasi database pada app.py, menghapus fitur 'Ask Budi' pada layout.html, dan membuat file exercises.html

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 import os
-import sqlalchemy
+from sqlalchemy import create_engine
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
+# Create Flask application
 app = Flask(__name__)
 
+# Configure session
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Configure sqlalchemy engine object to use sqlite database
+engine = create_engine("sqlite:///database.db")
+
+# Make sure API key is set
+if not os.environ.get("API_KEY"):
+    raise RuntimeError("API_KEY not set")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    return render_template("layout.html")
