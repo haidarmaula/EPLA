5d51797b28b1c1ac399a4f3eeb7e47b576fbc29e
haidarmaula <haidarmaulareynaldy@gmail.com>
2023-07-24 23:31:40 +0700

    Create login form

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def after_request(response):
 
 @app.route("/")
 def index():
-    return render_template("layout.html")
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
