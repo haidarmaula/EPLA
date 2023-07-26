fd0924eade9882760060e13d0b474abb42d4d701
haidarmaula <haidarmaulareynaldy@gmail.com>
2023-07-26 09:16:33 +0700

    Create index page and make Epla logo clickable

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def after_request(response):
 
 @app.route("/")
 def index():
-    return render_template("register.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
