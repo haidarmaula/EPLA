bee1efa7dfd760a68f2bea1c2732e33ae0c6609a
haidarmaula <haidarmaulareynaldy@gmail.com>
2023-07-27 21:07:21 +0700

    Configure exercises route and create results page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from sqlalchemy import create_engine
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
@@ -16,7 +17,9 @@ Session(app)
 engine = create_engine("sqlite:///database.db")
 
 # Make sure API key is set
-if not os.environ.get("API_KEY"):
+API_KEY = os.environ.get("API_KEY")
+
+if not API_KEY:
     raise RuntimeError("API_KEY not set")
 
 @app.after_request
@@ -27,8 +30,43 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route("/")
-def index():
+def is_sub_dict(sub_dict, main_dict):
+    """Determines whether a dict is a sub-dict of another dict"""
+    for key, value in sub_dict.items():
+        if main_dict[key] != value:
+            return False
+    return True
+
+@app.route("/exercises", methods=["GET", "POST"])
+def exercises():
+    if request.method == "POST":
+        api_url = "https://api.api-ninjas.com/v1/exercises?"
+
+        # Searches a list of exercises that match the user's search
+        if request.form.get("exercise-name"):
+            name = request.form.get("exercise-name")
+            results = requests.get(api_url + f"name={name}", headers={'X-Api-Key': API_KEY}).json()
+            return render_template("results.html", results=results)
+        
+        categories = request.form.to_dict()
+        exercises = []
+
+        # Searches the list of exercises that fit the category the user is looking for
+        for key, value in categories.items():
+            response = requests.get(api_url + f"{key}={value}", headers={'X-Api-Key': API_KEY}).json()
+            for exercise in response:
+                if exercise not in exercises:
+                    exercises.append(exercise)
+
+        results = []
+
+        # Selecting a list of exercises to exactly match the category the user is looking for
+        for exercise in exercises:
+            if is_sub_dict(categories, exercise):
+                results.append(exercise)
+
+        return render_template("results.html", results=results)
+
     return render_template("exercises.html")
 
 if __name__ == "__main__":
