22660572a2e128a4d9a1f5615b8d00f6bb55b8e8
haidarmaula <haidarmaulareynaldy@gmail.com>
2023-08-01 12:48:05 +0700

    Create database function to connect database

--- helpers.py
+++ helpers.py
@@ -1,6 +1,16 @@
+import sqlite3
+
+
 # Determines whether a dict is a sub-dict of another dict
 def is_sub_dict(sub_dict, main_dict):
     for key, value in sub_dict.items():
         if main_dict[key] != value:
             return False
     return True
+
+
+# Connect database
+def database(db="database.db"):
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+    return con, cur
