7bd3e5a5724b23cfa1b408d101f6baefa4d49c71
haidarmaula <haidarmaulareynaldy@gmail.com>
2023-08-01 12:50:08 +0700

    Create login_required function

--- helpers.py
+++ helpers.py
@@ -1,4 +1,15 @@
 import sqlite3
+from flask import session, redirect
+from functools import wraps
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 # Determines whether a dict is a sub-dict of another dict
