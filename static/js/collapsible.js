b95077e083141db388d0cf280af7893de7b686fd
haidarmaula <haidarmaulareynaldy@gmail.com>
2023-09-01 17:08:17 +0700

    Change the search result style

--- static/js/collapsible.js
+++ static/js/collapsible.js
@@ -1,12 +1,15 @@
-var collapsible = document.getElementsByName("collapsible");
+const exercises = document.getElementsByName("exercise");
         
-for (i = 0; i < collapsible.length; i++) {
-    collapsible[i].addEventListener("click", function() {
-        var content = this.nextElementSibling;
+exercises.forEach(exercise => {
+    exercise.addEventListener("click", function() {
+        const svgArrow = exercise.querySelector('svg');
+        svgArrow.classList.toggle('rotated');
+
+        const content = this.nextElementSibling;
         if (content.style.maxHeight){
             content.style.maxHeight = null;
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
         } 
     });
-}
+});
