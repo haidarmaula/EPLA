a4c56ccc7425c083640b76c7466840bc450838b3
haidarmaula <haidarmaulareynaldy@gmail.com>
2023-09-11 13:08:39 +0700

    Create sidebar for mobile devices

---
+++ static/js/sidebar.js
@@ -0,0 +1,6 @@
+const barsIcon = document.querySelector('[name="bars-icon"]');
+const sidebars = document.querySelector('[name="sidebar"]')
+
+barsIcon.addEventListener('click', (event) => {
+    sidebars.classList.toggle('hidden');
+});
